8c1f2683bf0eddaa4e4e06a6bfae8ae78d464313
robertotambunan <robertotambunan14@gmail.com>
2017-06-23 01:01:32 +0700

    how to use package

--- src/github.com/user/hello/hello.go
+++ src/github.com/user/hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
 import "fmt"
+import "github.com/user/stringfunction"
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+	fmt.Print("Reverse The Word: ")
+	fmt.Println(stringfunction.Reverse("Hello World"))
 }
